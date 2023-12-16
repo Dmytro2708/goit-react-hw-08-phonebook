12b50b112345b870185f352711ba453b2e6d47e9
Dmytro2708 <130864203+Dmytro2708@users.noreply.github.com>
2023-12-16 22:20:46 +0000

    Deploying to gh-pages from @ Dmytro2708/goit-react-hw-08-phonebook@352436416aae2eb8d4b053077bf804b633433e71 🚀

---
+++ static/js/206.e30e2fb1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[206],{1206:function(e,t,n){n.r(t);var i=n(9482),l=n(184),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};t.default=function(){return(0,l.jsxs)("div",{style:c.container,children:[(0,l.jsx)("h1",{style:c.title,children:" Contacts manager welcome page"}),(0,l.jsx)(i.A_l,{fill:"#ff923cba",fontSize:"2em"})]})}}}]);
+//# sourceMappingURL=206.e30e2fb1.chunk.js.map
