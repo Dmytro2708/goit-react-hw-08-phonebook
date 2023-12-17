88612fb7865b5935d85152068611997a75db813f
Dmytro2708 <130864203+Dmytro2708@users.noreply.github.com>
2023-12-17 16:19:52 +0200

    styled

--- src/components/NameInput/NameInput.styled.js
+++ src/components/NameInput/NameInput.styled.js
@@ -2,33 +2,41 @@ import styled from 'styled-components';
 import { Form } from 'formik';
 
 export const StyledForm = styled(Form)`
-  display: flex;
+   display: flex;
   flex-direction: column;
-  width: 300px;
+  width: 400px;
+  padding: 10px;
   outline: 1px solid grey;
   border-radius: 20px;
-  padding: 10px;
   background-color: #d3d3d3;
+  margin: 0 auto;
   label {
     display: flex;
-    flex-direction: column;
-    margin-bottom: 10px;
+  flex-direction: column;
+  margin-bottom: 16px;
+  font-size: 16px;
   }
   input {
-    margin-top: 6px;
-    border-radius: 20px;
+    padding: 4px 12px;
+  border-radius: 20px;
+  font: inherit;
   }
   button {
     width: 100px;
-    cursor: pointer;
-    background-color: orange;
-    border-radius: 5px;
-    border: none;
-    box-shadow: 0 1px 7px grey;
-    &:hover {
-      background-color: blueviolet;
-      color: #fff;
-      transform: scale(1.1);
+  padding: 5px;
+  cursor: pointer;
+  border-radius: 5px;
+  border: none;
+  box-shadow: 0 1px 7px grey;
+  background-color: orange;
+  margin-right: 4px;
+  box-shadow: 0 1px 7px grey;
+
+  &:hover,
+  &:focus {
+    background-color: blueviolet;
+    color: #fff;
+    transform: scale(1.1);
     }
   }
 `;
